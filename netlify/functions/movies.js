0e04a8aeb07e438e2f738f61a9f224143c5a2f76
Marcos Cogo <marcos.cogo@kellogg.northwestern.edu>
2021-05-10 18:41:39 -0500

    hw6

--- netlify/functions/movies.js
+++ netlify/functions/movies.js
@@ -48,6 +48,7 @@ exports.handler = async function(event) {
         returnValue.movies.push(movie)
     }
 
+    
     // a lambda function returns a status code and a string of data
     return {
       statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
